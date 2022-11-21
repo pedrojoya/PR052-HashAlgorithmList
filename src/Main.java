5db12bbdebba9698da9c213de77b1039e950df97
pedrojoya <pedrojoya@gmail.com>
2022-11-21 18:44:06 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.security.MessageDigest;
+import java.security.Provider;
+import java.security.Security;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Arrays.stream(Security.getProviders())
+                .flatMap(provider -> provider.getServices().stream())
+                .filter(service -> service.getType().equals(MessageDigest.class.getSimpleName()))
+                .map(Provider.Service::getAlgorithm)
+                .forEach(System.out::println);
+    }
+}
