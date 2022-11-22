41297ec2bf41e0b151a6f3c64a71baebd485681f
pedrojoya <pedrojoya@gmail.com>
2022-11-22 18:37:16 +0100

    Print each algorithm in a line. Use distinct and sorted

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ import java.security.MessageDigest;
 import java.security.Provider;
 import java.security.Security;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        String hashAlgorithms = Arrays.stream(Security.getProviders())
+        Arrays.stream(Security.getProviders())
                 .flatMap(provider -> provider.getServices().stream())
                 .filter(service -> service.getType().equals(MessageDigest.class.getSimpleName()))
                 .map(Provider.Service::getAlgorithm)
-                .collect(Collectors.joining(", "));
-        System.out.printf("Hash algorithms: %s\n", hashAlgorithms);
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
     }
 }
