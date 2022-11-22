6c25b9bf6d5922c41583a4c5e5f07192c0a169c8
pedrojoya <pedrojoya@gmail.com>
2022-11-22 07:40:59 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ import java.security.MessageDigest;
 import java.security.Provider;
 import java.security.Security;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        Arrays.stream(Security.getProviders())
+        String hashAlgorithms = Arrays.stream(Security.getProviders())
                 .flatMap(provider -> provider.getServices().stream())
                 .filter(service -> service.getType().equals(MessageDigest.class.getSimpleName()))
                 .map(Provider.Service::getAlgorithm)
-                .forEach(System.out::println);
+                .collect(Collectors.joining(", "));
+        System.out.printf("Hash algorithms: %s\n", hashAlgorithms);
     }
 }
